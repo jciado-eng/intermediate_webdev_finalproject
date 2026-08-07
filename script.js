c01699ad7c04fee8b0ac2afb0b426ddf42b34049
jciado-eng <jhonnyciado@gmail.com>
2026-08-07 17:32:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const calculateSimpleInterest = (principal, rate, time) => {
     return (principal * rate * time) / 100;
-  }
+}
 
-  const calculateTotalPayableAmount = (principal, interestAmount) => {
-    return principal - interestAmount;
-  }
-   
+const calculateTotalPayableAmount = (principal, interestAmount) => {
+    return principal + interestAmount;
+}
 
-  const calculate = () => {
+const calculate = () => {
     let p = Number(document.getElementById("principal").value);
     let r = Number(document.getElementById("rate").value);
     let t = Number(document.getElementById("time").value);
@@ -19,7 +18,8 @@ const calculateSimpleInterest = (principal, rate, time) => {
     result.innerHTML = `<div>Principal Amount: <span>${p.toFixed(2)}</span></div>
     <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
     <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
-  };
+};
   
-  if (typeof module !== 'undefined')
-  module.exports = { calculateSimpleInterest, calculateTotalPayableAmount, calculate };
+if (typeof module !== 'undefined') {
+    module.exports = { calculateSimpleInterest, calculateTotalPayableAmount, calculate };
+}
